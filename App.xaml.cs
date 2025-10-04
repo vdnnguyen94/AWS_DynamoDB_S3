5bc0f65dbb8c3ff5f28620073029da75711692f9
Van Nguyen <vdnnguyen94@gmail.com>
2025-10-04 08:51:38 -0400

    project initiation

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _301289600Nguyen_Lab2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
